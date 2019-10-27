9b7cc0cc947c5e1ee41a280fc98f947869045bc6
leonidshparun <leonidshparun@gmail.com>
2019-10-27 17:02:01 +0300

    feat: add base footer markup

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
+import Section from 'src/shared/Section/Section';
+
 import styles from './style.module.scss';
 
-export default () => <footer className={styles.container}>FOOTER</footer>;
+export default () => (
+  <footer className={styles.container}>
+    <Section label="Contact us" heading="Let’s get started with CleverSoft!">
+      <div>CONTENT</div>
+    </Section>
+  </footer>
+);
