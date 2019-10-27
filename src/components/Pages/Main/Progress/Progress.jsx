db2a54efc8ca391514badf0c226786b36bac6589
leonidshparun <leonidshparun@gmail.com>
2019-10-27 12:17:25 +0300

    feat: layout for progress section

---
+++ src/components/Pages/Main/Progress/Progress.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import styles from './style.module.scss';
+
+const progress = [
+  { value: '20', label: 'years of experience in the IT field' },
+  { value: '250+', label: 'in-house experts' },
+  { value: '500+', label: 'clients across the globe' },
+  { value: '840', label: 'projects in different business domains' }
+];
+
+export default () => (
+  <section className={styles.container}>
+    <div className={styles.wrapper}>
+      <ul className={styles.progress__container}>
+        {progress.map(item => (
+          <li className={styles.progress__item}>
+            <p className={styles.progress__item_value}>{item.value}</p>
+            <p className={styles.progress__item_label}>{item.label}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  </section>
+);
