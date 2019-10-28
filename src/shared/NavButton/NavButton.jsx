8f40975cfe3ae8913872b97eb4ab43038cd7c4f2
leonidshparun <leonidshparun@gmail.com>
2019-10-28 14:52:18 +0300

    feat: add new type for button

--- src/shared/NavButton/NavButton.jsx
+++ src/shared/NavButton/NavButton.jsx
@@ -3,8 +3,11 @@ import React from 'react';
 import { navigate } from 'gatsby';
 import styles from './style.module.scss';
 
-export default ({ text, link }) => (
-  <a className={styles.button} onClick={() => navigate(link)}>
-    <span className={styles.button__text}>{text}</span>
-  </a>
-);
+export default ({ text, link, type }) => {
+  const className = `${styles.button} ${styles[type]}`;
+  return (
+    <a className={className} onClick={() => navigate(link)}>
+      <span className={styles.button__text}>{text}</span>
+    </a>
+  );
+};
