6da34f2f8ddad9a19a1ced061d3cf62a0f9043cf
leonidshparun <leonidshparun@gmail.com>
2019-10-28 14:53:21 +0300

    feat: styling for navigation component

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
-export default ({ menuLinks }) => (
-  <nav>
-    <ul>
-      {menuLinks.map(link => (
-        <li key={link.name}>
-          <Link to={link.link}>{link.name}</Link>
-        </li>
-      ))}
-    </ul>
-  </nav>
-);
+import NavButton from 'src/shared/NavButton/NavButton';
+import styles from './style.module.scss';
+
+export default ({ menuLinks }) => {
+  const navLinks = menuLinks.slice(0, menuLinks.length - 1);
+  const extra = menuLinks[menuLinks.length - 1];
+  return (
+    <nav className={styles.navigation}>
+      <ul className={styles.navigation__links}>
+        {navLinks.map(link => (
+          <li key={link.name}>
+            <Link activeStyle={{ fontWeight: 600 }} to={link.link}>
+              {link.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <NavButton text={extra.name} link={extra.link} type="b" />
+    </nav>
+  );
+};
