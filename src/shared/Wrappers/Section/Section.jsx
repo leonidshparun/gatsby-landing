241a52881a8eb26ed6e015cd7a319891e24ed737
leonidshparun <leonidshparun@gmail.com>
2019-10-29 22:22:56 +0300

    fix: section style override

--- src/shared/Wrappers/Section/Section.jsx
+++ src/shared/Wrappers/Section/Section.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 import styles from './style.module.scss';
 
-export default ({ children, label, heading }) => (
-  <section id={`${label}`.toLowerCase()} className={styles.container}>
+export default ({ children, label, heading, css }) => (
+  <section
+    id={`${label}`.toLowerCase()}
+    className={`${styles.container} ${css}`}
+  >
     <div className={styles.wrapper}>
       <div className={styles.label}>{label}</div>
       <h2> {heading}</h2>
