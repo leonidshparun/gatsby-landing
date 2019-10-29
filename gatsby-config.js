66d4edd65b0dd091a86b8dc95a52fc9d9d423174
leonidshparun <leonidshparun@gmail.com>
2019-10-29 16:06:40 +0300

    feat: contentful cms support

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,28 @@
+if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config();
+}
+
 module.exports = {
-  plugins: ['gatsby-plugin-root-import', `gatsby-plugin-sass`],
+  plugins: [
+    'gatsby-plugin-root-import',
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+      }
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: './data/'
+      }
+    },
+    'gatsby-transformer-json',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp'
+  ],
   siteMetadata: {
     title: 'CleverSoft',
     menuLinks: [
