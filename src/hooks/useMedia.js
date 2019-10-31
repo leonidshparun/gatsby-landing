08e2481bc933e342ae26618df93e5aaddeb58511
leonidshparun <leonidshparun@gmail.com>
2019-10-31 20:32:20 +0300

    fix: window build error

--- src/hooks/useMedia.js
+++ src/hooks/useMedia.js
@@ -2,8 +2,10 @@ import { useState, useEffect, useCallback } from 'react';
 
 export default (queries, values, defaultValue) => {
   // Array containing a media query list for each query
-  const mediaQueryLists = queries.map(q => window.matchMedia(q));
-
+  let mediaQueryLists = [];
+  if (window !== 'undefined') {
+    mediaQueryLists = queries.map(q => window.matchMedia(q));
+  }
   // Function that gets value based on matching media query
   const getValue = useCallback(() => {
     // Get index of first media query that matches
