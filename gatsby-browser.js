d57e5dc68d8f1a1123995ba1f5f8912bf23bacf8
Leonid Shparun <47000975+leonidshparun@users.noreply.github.com>
2019-11-02 01:18:41 +0300

    fix: scroll bug with anchors

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,11 @@
-import './src/styles/global.css';
+exports.shouldUpdateScroll = ({
+  routerProps: { location },
+  getSavedScrollPosition
+}) => {
+
+  if (location.hash) {
+    return false;
+  }
+
+  return true;
+};
