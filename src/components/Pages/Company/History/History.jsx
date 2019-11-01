7e3c48367100cde5b85f080d83c3d42771b7210c
leonidshparun <leonidshparun@gmail.com>
2019-11-01 18:45:45 +0300

    feat: add history section

---
+++ src/components/Pages/Company/History/History.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import Section from 'src/shared/Wrappers/Section/Section';
+
+import styles from './style.module.scss';
+
+export default () => (
+  <Section label="About us" heading="Our history" css={styles.section}>
+    <div className={styles.container}>
+      <p>
+        Founded in 2001, CleverSoft acted as a product owner of a scientific
+        system for our first client that had reached out to us because they
+        needed guidance and necessary expertise to do that. We created
+        requirements, established development processes and standards, and
+        partly planned the marketing strategy. The product turned out to be
+        innovative, and we built a computer-based training system for users, as
+        well as established benchmarks to improve user experience. That product
+        was a big win for both CleverSoft and our customer.
+      </p>
+    </div>
+  </Section>
+);
