fbbd34aa2fc23a9aaa8653af9ce35bc71782955f
Leonid Shparun <47000975+leonidshparun@users.noreply.github.com>
2019-11-02 01:29:32 +0300

    Update gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,14 @@
-//
+import './src/styles/global.css';	
+
+exports.shouldUpdateScroll = ({
+exports.shouldUpdateScroll = ({
+  routerProps: { location },
+  getSavedScrollPosition
+}) => {
+
+  if (location.hash) {
+    return false;
+  }
+
+  return true;
+};
