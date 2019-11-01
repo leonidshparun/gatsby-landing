fddd67b2a96bbb1ab2070ba212a8d41a0b0d6ee2
leonidshparun <leonidshparun@gmail.com>
2019-11-01 21:35:43 +0300

    refactor: more flexible component

--- src/shared/Wrappers/Section/Section.jsx
+++ src/shared/Wrappers/Section/Section.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 import styles from './style.module.scss';
 
-export default ({ children, label, heading, css }) => (
+export default ({ children, label, heading, css, noLabel, noHeading }) => (
   <section
     id={`${label}`.toLowerCase()}
     className={`${styles.container} ${css}`}
   >
     <div className={styles.wrapper}>
-      <div className={styles.label}>{label}</div>
-      <h2>{heading}</h2>
+      {!noLabel && <div className={styles.label}>{label}</div>}
+      {!noHeading && <h2>{heading}</h2>}
       {children}
     </div>
   </section>
