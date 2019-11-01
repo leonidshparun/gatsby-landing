789fe9130249bbd721c393156796fe06a9cff711
leonidshparun <leonidshparun@gmail.com>
2019-11-01 20:17:21 +0300

    refactor: separate slider logic

---
+++ src/shared/Sliders/SliderContent/Line/SliderContent.jsx
@@ -0,0 +1,62 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React, { Component } from 'react';
+
+import uniqid from 'uniqid';
+import Slider from 'react-slick';
+
+import Button from '../../Button/Button';
+import Frame from '../Frame/Frame';
+
+import styles from './style.module.scss';
+
+export default class SlickSLider extends Component {
+  render() {
+    const slidesToShow = 5;
+
+    const { items, withFrame, action } = this.props;
+    const settings = {
+      dots: false,
+      arrows: false,
+      infinite: true,
+      speed: 500,
+      slidesToShow,
+      slidesToScroll: 1,
+      beforeChange: action || null
+    };
+
+    return (
+      <div className={styles.container}>
+        <Button
+          action={() => {
+            this.slider.slickPrev();
+          }}
+        />
+        <div style={{ width: '80%', zIndex: 1 }}>
+          <Slider
+            ref={c => {
+              this.slider = c;
+            }}
+            {...settings}
+            style={{ display: 'inline' }}
+          >
+            {items.map((item, idx) => (
+              <div index={idx} key={uniqid()}>
+                <div style={{ textAlign: 'center' }}>{item.company.name}</div>
+              </div>
+            ))}
+          </Slider>
+        </div>
+        <Button
+          action={() => {
+            this.slider.slickNext();
+          }}
+        />
+        {withFrame ? (
+          <div className={styles.frame}>
+            <Frame size="16rem" color="#ccc" />
+          </div>
+        ) : null}
+      </div>
+    );
+  }
+}
--- src/shared/Sliders/SliderContent/SliderContent.jsx
+++ src/shared/Sliders/SliderContent/SliderContent.jsx
@@ -1,15 +1,8 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React, { Component } from 'react';
 
-import uniqid from 'uniqid';
-import Slider from 'react-slick';
-
 import FeedbackBlock from 'src/shared/Blocks/FeedbackBlock/FeedbackBlock';
-
-import Button from '../Button/Button';
-import Frame from './Frame/Frame';
-
-import styles from './style.module.scss';
+import Line from './Line/SliderContent';
 
 export default class SlickSLider extends Component {
   constructor(props) {
@@ -23,15 +16,8 @@ export default class SlickSLider extends Component {
     const slidesToShow = 5;
     const middleIndex = Math.floor(slidesToShow / 2);
 
-    const settings = {
-      dots: false,
-      arrows: false,
-      infinite: true,
-      speed: 500,
-      slidesToShow,
-      slidesToScroll: 1,
-      beforeChange: (current, next) => this.setState({ activeSlide: next })
-    };
+    const updateFeedbackBlock = (current, next) =>
+      this.setState({ activeSlide: next });
 
     const { items } = this.props;
     const { activeSlide } = this.state;
@@ -40,36 +26,7 @@ export default class SlickSLider extends Component {
 
     return (
       <>
-        <div className={styles.container}>
-          <Button
-            action={() => {
-              this.slider.slickPrev();
-            }}
-          />
-          <div style={{ width: '80%', zIndex: 1 }}>
-            <Slider
-              ref={c => {
-                this.slider = c;
-              }}
-              {...settings}
-              style={{ display: 'inline' }}
-            >
-              {items.map((item, idx) => (
-                <div index={idx} key={uniqid()}>
-                  <div style={{ textAlign: 'center' }}>{item.company.name}</div>
-                </div>
-              ))}
-            </Slider>
-          </div>
-          <Button
-            action={() => {
-              this.slider.slickNext();
-            }}
-          />
-          <div className={styles.frame}>
-            <Frame size="16rem" color="#ccc" />
-          </div>
-        </div>
+        <Line withFrame action={updateFeedbackBlock} items={items} />
 
         <FeedbackBlock data={items[current]} />
       </>
