eb35ad3ddda76646b46fb0bc04d41995386be144
leonidshparun <leonidshparun@gmail.com>
2019-11-01 18:47:49 +0300

    refactor: project structure

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,7 +4,7 @@ import Layout from 'src/components/Layout/Layout';
 
 import Board from 'src/components/Pages/Main/Board/Board';
 import Industries from 'src/components/Pages/Main/Industries/Industries';
-import Progress from 'src/components/Pages/Main/Progress/Progress';
+import Progress from 'src/components/Progress/Progress';
 import Testimonials from 'src/components/Pages/Main/Testimonials/Testimonials';
 import Portfolio from 'src/components/Pages/Main/Portfolio/Portfolio';
 import AboutUs from 'src/components/Pages/Main/AboutUs/AboutUs';
