d86a4ef73d67da3bedc93b4d3236278f3b577041
leonidshparun <leonidshparun@gmail.com>
2019-11-03 11:41:09 +0300

    refactor: push stack data for contentful

--- src/components/Stack/Stack.jsx
+++ src/components/Stack/Stack.jsx
@@ -1,32 +1,48 @@
 import React from 'react';
 
+import { StaticQuery, graphql } from 'gatsby';
+
 import Section from 'src/shared/Wrappers/Section/Section';
 import SliderContent from 'src/shared/Sliders/SliderContent/Line/SliderContent';
 
 import styles from './style.module.scss';
 
-function importAll(r) {
-  const images = {};
-  r.keys().forEach(item => {
-    images[item.replace('./', '')] = r(item);
-  });
-  return images;
-}
-
-const images = importAll(
-  require.context('src/assets/pics/tech', false, /\.(png|jpe?g|svg)$/)
+export default () => (
+  <StaticQuery
+    query={graphql`
+      query TechStack {
+        allContentfulTechStack {
+          edges {
+            node {
+              title
+              logo {
+                fluid(maxWidth: 200) {
+                  src
+                }
+              }
+            }
+          }
+        }
+      }
+    `}
+    render={data => {
+      const {
+        allContentfulTechStack: { edges }
+      } = data;
+      const images = edges.map(edge => {
+        const {
+          logo: { fluid },
+          title
+        } = edge.node;
+        return <img src={fluid.src} alt={title} />;
+      });
+      return (
+        <Section label="Tech stack" heading="Technologies we use">
+          <div className={styles.container}>
+            <SliderContent items={images} />
+          </div>
+        </Section>
+      );
+    }}
+  />
 );
-
-const stackData = Object.entries(images).map(image => (
-  <img src={image[1]} alt={image[0]} />
-));
-
-export default () => {
-  return (
-    <Section label="Tech stack" heading="Technologies we use">
-      <div className={styles.container}>
-        <SliderContent items={stackData} />
-      </div>
-    </Section>
-  );
-};
