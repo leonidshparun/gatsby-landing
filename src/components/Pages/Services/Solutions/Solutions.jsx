a9c102f299bebdc3361aff6161e1db37d20e0b31
leonidshparun <leonidshparun@gmail.com>
2019-11-05 14:41:27 +0300

    feat: connect cards to contentful cms

--- src/components/Pages/Services/Solutions/Solutions.jsx
+++ src/components/Pages/Services/Solutions/Solutions.jsx
@@ -1,43 +1,39 @@
 import React from 'react';
 
+import { useStaticQuery, graphql } from 'gatsby';
+
 import Section from 'src/shared/Wrappers/Section/Section';
 import Plates from 'src/components/Plates/Plates';
 
 import styles from './style.module.scss';
 
-const platesData = [
-  {
-    title: 'Mobile Applications',
-    link: '/',
-    description:
-      'We help you go mobile by creating unique apps with cross-device compatibility and an active layout. We also provide a cross-operating system check with our proprietary compatibility matrix. Our experts know how to turn the customer’s ideas into mobile apps that are easy to use, look appealing and smoothly perform on any device.'
-  },
-  {
-    title: 'Web Applications',
-    link: '/',
-    description:
-      'CleverSoft provides appealing web applications that offer high scalability, responsive design, and rich functionality. We always stay up to date with the latest web development trends and technologies to develop CMS-based and custom websites, web portals, web-enabled custom business applications, and web services.'
-  },
-  {
-    title: 'Desktop Applications',
-    link: '/',
-    description:
-      'Whatever your corporate environment, hardware and OS in place, we employ the appropriate technology, framework and language to deliver a turnkey solution. We create cross-platform enterprise desktop software, utilities, and plug-ins. To address your needs we develop custom applications such as document and knowledge management software, BI solutions and data analysis systems, ERP and CRM systems.'
-  },
-  {
-    title: 'Cloud Applications',
-    link: '/',
-    description:
-      'Businesses that want to gain extra reliability, scalability and data security, cut costs and boost mobility migrate to the cloud. We develop systems that are optimized for specific cloud service providers (Azure, AWS, Google cloud, etc.). We also develop cloud-based platforms and provide cloud migration services with complete or partial system reengineering, maintenance, and support services.'
-  }
-];
-
-export default () => (
-  <Section
-    label="Solutions"
-    heading="We provide custom software development of"
-    css={styles.container}
-  >
-    <Plates content={platesData} />
-  </Section>
-);
+export default () => {
+  const { allContentfulCard } = useStaticQuery(graphql`
+    {
+      allContentfulCard(filter: { type: { eq: "services_solutions" } }) {
+        edges {
+          node {
+            title
+            description {
+              description
+            }
+          }
+        }
+      }
+    }
+  `);
+  const data = allContentfulCard.edges.map(item => ({
+    title: item.node.title,
+    description: item.node.description.description,
+    link: '/'
+  }));
+  return (
+    <Section
+      label="Solutions"
+      heading="We provide custom software development of"
+      css={styles.container}
+    >
+      <Plates content={data} />
+    </Section>
+  );
+};
--- src/components/Pages/Solutions/Solutions/Solutions.jsx
+++ src/components/Pages/Solutions/Solutions/Solutions.jsx
@@ -1,61 +1,39 @@
 import React from 'react';
 
+import { useStaticQuery, graphql } from 'gatsby';
+
 import Section from 'src/shared/Wrappers/Section/Section';
 import Plates from 'src/components/Plates/Plates';
 
 import styles from './style.module.scss';
 
-const platesData = [
-  {
-    title: ' ERP Systems  ',
-    link: '/',
-    description:
-      ' ERP Systems enterprise software development for various industry verticals, having learned all the requirements, standards and minor nuances, which helps us successfully share our knowledge of enterprise resource planning. We develop custom ERP software, build their individual components, and offer implementation services. Our custom ERP software allows getting a set of business management tools and brings value to every functional area of enterprise.'
-  },
-  {
-    title: '  Document Management Systems ',
-    link: '/',
-    description:
-      'Document management software converts documents into valuable data that are subsequently used in the workflow. This software also speeds up decision-making and helps businesses go paperless thus making their workflow more secure and cheaper. We deliver custom systems that help manage complex business processes, provide complete visibility of the company’s document flow and productivity of the personnel.'
-  },
-  {
-    title: '  Data Storage and Archiving Software ',
-    link: '/',
-    description:
-      'Enterprises produce and utilize large amounts of data and face the problems of data storage and archiving. Our software engineers have first-hand experience in building secure and expandable storage software that enables long-term data retention and frees up operational storage space, which eventually improves workflow efficiency and flexibility and reduces costs.'
-  },
-  {
-    title: ' Infrastructure Management   ',
-    link: '/',
-    description:
-      'Enterprises have a complex infrastructure to manage and monitor. In the cases when even implementing inventory management is problematic, infrastructure management software come in handy. Such software connects all the systems and employees within an organization, supports business transactions and processes, ensures data storage, processing, reporting, and more.'
-  },
-  {
-    title: ' CRM  ',
-    link: '/',
-    description:
-      'Сustomer relationship management systems assist marketing, sales and business development departments in sales enablement, lead management, marketing automation, customer service improvement, data analytics, reporting, etc. We build CRM systems from scratch as well as customize, integrate and re-implement the existing systems.'
-  },
-  {
-    title: '  Logistics & Supply Chain Management  ',
-    link: '/',
-    description:
-      'As a technology leader, CleverSoft understands the needs of logistics specialists and specifics of logistics departments within large companies. We integrate logistics software into the single corporate ERP system that helps manage logistics and transportation as well as engages third-party logistics services.'
-  },
-  {
-    title: ' Financial Software ',
-    link: '/',
-    description:
-      'Financial departments of large enterprises require sophisticated and secure analytical software to analyze sales, cash flow, overall return, and operating income. This software identifies fraud and ensures an operational and strategic analysis and real-time analytics. Such systems also provide assistance in making accurate forecasts and taking strategic decisions.'
-  }
-];
-
-export default () => (
-  <Section
-    label="Services"
-    heading="Custom software development services we offer"
-    css={styles.container}
-  >
-    <Plates content={platesData} wide />
-  </Section>
-);
+export default () => {
+  const { allContentfulCard } = useStaticQuery(graphql`
+    {
+      allContentfulCard(filter: { type: { eq: "solutions_services" } }) {
+        edges {
+          node {
+            title
+            description {
+              description
+            }
+          }
+        }
+      }
+    }
+  `);
+  const data = allContentfulCard.edges.map(item => ({
+    title: item.node.title,
+    description: item.node.description.description,
+    link: '/'
+  }));
+  return (
+    <Section
+      label="Services"
+      heading="Custom software development services we offer"
+      css={styles.container}
+    >
+      <Plates content={data} wide />
+    </Section>
+  );
+};
